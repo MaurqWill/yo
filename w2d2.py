1dfb3fdc03f8b46acbba6ee41fce2f3b3438c12d
MaurqWill <mwilliams.mw74@gmail.com>
2024-03-19 19:02:26 -0400

    homework

---
+++ w2d2.py
@@ -0,0 +1,51 @@
+# # Decisions at the Crossroad
+# number = input("Enter a number: ")
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number == 0:
+#     print("The number is zero.")
+# else:
+#     print("The number is negative.")
+
+
+# user_input = input("Give me three numbers!")
+
+
+
+# Leap Year Explorer
+
+
+
+# # Nested Decisions: The Adventure Game
+
+# place = input("Choose a place: forest or cave? ")
+
+# if place == "forest":
+#     action = input("climb a tree or cross a river?")
+#     if action == "climb a tree":
+#         print("You found a bird's nest!")
+#     else: "cross a river"
+#     print("You found a boat!")
+# elif place == "cave":
+#     action = input("Do you want to light a torch or proceed in the dark?")
+#     if action == "light a torch":
+#         print("You find a hidden treasure!")
+#     else: "proceed in the dark"
+#     print("find the treasure!")
+# def place():
+#     pass
+
+#  Quick Decisions: The Event Planner
+
+attendees = input("Enter number of attendees: ")
+venue = "large hall" 
+print(venue) if attendees > 100 else print("conference room")
+add_ons = "audio_system", "projector"
+if venue:
+    print("would you like " + "add_ons")
+food = input("Do you want vegetarian food yes or no?")
+yes = food
+if yes: print("You should try Veggie Delight Caterers") 
+else:
+ print("You should try Gourmet Meals Caterers")
